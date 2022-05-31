20c4b6b8f828a952deed2bff17af819959264c98
pplknomeasb <76265895+pplknomeasb@users.noreply.github.com>
2022-05-31 15:39:36 -0400

    Add files via upload

---
+++ variableDeclaration.py
@@ -0,0 +1,11 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+#Defining a variable
+my_var = 10
+my_var = 10 #Type integar
+my_car = "Hello" #type string
+my_car = True #type boolean
+#Variables types are assigned by Python automatically
+a=b=c=10 #a, b, and c are 3 difference integar variables that have the same value of 10
+a,b,c = 1, 2, 3 #a = 1, b = 2, c = 3
+
