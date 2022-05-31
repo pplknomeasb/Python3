3eb5c881690b59be0cb4a8ac8341825beb034a6b
pplknomeasb <76265895+pplknomeasb@users.noreply.github.com>
2022-05-31 16:07:35 -0400

    Add files via upload

---
+++ variableTypes.py
@@ -0,0 +1,18 @@
+#String datatypes are structures used in the vast majority of programs.
+#Not only are string types used to store data but they are also used to
+#reference file types ex name = brennan
+
+
+#numbers ex price = 125.3
+
+#boolean result = ture how = false the values can be used to alter the output of the program
+
+# list colors = ["red", "blue", "green", "yellow"] the difference between lists and sets is sets have Zero duplicates
+
+#Sets colors = {"red", blue", greene", "yellow"} the difference between lists and sets is sets have ZERO duplicates
+
+#Tuples colors = ("red", "blue", "green", "yellow") once intitiated, cannot be changed.
+
+#Range age = range(21, 60)
+
+#dictionaries employees={"John": "Sales", "Maria": "Legal", "Lara": "HR"}
